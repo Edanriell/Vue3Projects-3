9870286f8b87f3f78d35e47e643979bc6a30e721
Edanriell <Edanriell@gmail.com>
2024-08-12 16:18:01 +0300

    final cleanup

--- UpscaleSpa/src/main.ts
+++ UpscaleSpa/src/main.ts
@@ -1,10 +1,10 @@
-import { createApp } from "vue";
+import {createApp} from "vue";
 import "./style.css";
 import App from "./App.vue";
 
 // Check if browser supports service workers
 if (navigator.serviceWorker) {
-	navigator.serviceWorker.register("/service_worker.js");
+	navigator.serviceWorker.register("/service_worker.ts");
 }
 
 createApp(App).mount("#app");
